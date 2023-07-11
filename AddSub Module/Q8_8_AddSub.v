7350bb0e45e303460df5405ded0bc27b6826a40c
Saicharan Ritwik Chinni <109512004+SaicharanRitwik39@users.noreply.github.com>
2023-07-12 00:02:08 +0530

    Update Q8_8_AddSub.v

--- AddSub Module/Q8_8_AddSub.v
+++ AddSub Module/Q8_8_AddSub.v
@@ -6,32 +6,20 @@ module Q8_8_AddSub
   (input wire [BUS_WIDTH-1:0] operand1,
    input wire [BUS_WIDTH-1:0] operand2,
    input add_sub,  // 0 for Addition, 1 for Subtraction.
-   output wire [BUS_WIDTH:0] result
+   output reg [BUS_WIDTH:0] result
 );
-  
-  reg [(BUS_WIDTH/2)-1:0] integer_sum;
-  reg [(BUS_WIDTH/2)-1:0] fraction_sum;
 
   always @* begin
-    if (add_sub == 0) begin  // Addition
-      integer_sum = operand1[15:8] + operand2[15:8];
-      fraction_sum = operand1[7:0] + operand2[7:0];
+    if (add_sub == 0) begin  // Addition.
+      result = operand1+operand2;
     end
-    else begin  // Subtraction
-      integer_sum = operand1[15:8] - operand2[15:8];
-      fraction_sum = operand1[7:0] - operand2[7:0];
+    else begin  // Subtraction.
+      if (operand1 > operand2) begin
+        result = operand1-operand2;
+      end  
+      else begin result = operand2-operand1;
+      end
     end
 
-    if (fraction_sum >= 256) begin
-      integer_sum = integer_sum + 1;
-      fraction_sum = fraction_sum - 256;
-    end
-    else if (fraction_sum < 0) begin
-      integer_sum = integer_sum - 1;
-      fraction_sum = fraction_sum + 2556;
-    end
   end
-  assign result = {integer_sum, fraction_sum};
-
 endmodule
-
